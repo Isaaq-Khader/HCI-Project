a9a3fa67a56342601ad3b98ad8b2664f97542585
Isaaq-Khader <isaaqkhader@uchicago.edu>
2022-10-16 21:09:45 -0500

    pushing files

---
+++ app/src/main/java/com/example/hciproject1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hciproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView=findViewById(R.id.webView);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("/app/src/main/assets/index.html");
+
+    }
+}
