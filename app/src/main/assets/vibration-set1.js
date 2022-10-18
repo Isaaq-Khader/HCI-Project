254b651d22e7e6472032a3e9175c88c0c020f6d7
Dam Son <damchaus1@Chausons-MacBook-Pro.local>
2022-10-18 17:32:41 -0500

    added swipes to vibration set1 and set2

--- app/src/main/assets/vibration-set1.js
+++ app/src/main/assets/vibration-set1.js
@@ -8,7 +8,55 @@
 //    }
 //  });
 //
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
 
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */
+            console.log(xDiff);
+        } else {
+            /* left swipe */
+            console.log(xDiff);
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */
+            console.log(yDiff);
+        } else {
+            /* up swipe */
+            console.log(yDiff);
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
 
 function like() {
     navigator.vibrate([150, 100, 100]);
